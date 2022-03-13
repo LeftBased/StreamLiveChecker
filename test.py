2d92b6637d38703870303df85cc07af9fe852db3
LeftBased <59938062+LeftBased@users.noreply.github.com>
2022-03-13 04:11:53 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,6 @@
 from urllib.request import urlopen
 StreamerStatus = []
-def streamerList(file_path, stream_platform):
-    if stream_platform == '' or None:
-        print("Error: No stream platform defined.")
-        return
+def TwitchStreamerList(file_path):
     if file_path == '' or None:
         print("Error: No streamer list selected.")
         return
@@ -12,14 +9,36 @@ def streamerList(file_path, stream_platform):
         list1 = textData.read().split('\n')
         for i in range(len(list1)):
             streamer = list1[i]
-            if stream_platform == 0:
-                X = CheckTwitchStream(streamer)
-                if X == True:
-                    StreamerStatus.append(streamer + " : ONLINE")
-                else:
-                    StreamerStatus.append(streamer + " : OFFLINE")
-        with open("Streamer_Status.txt","a") as file:
+            X = CheckTwitchStream(streamer)
+            if X == True:
+                StreamerStatus.append(streamer + " : ONLINE")
+            else:
+                StreamerStatus.append(streamer + " : OFFLINE")
+        with open("streamers_twitch_status.txt","a") as file:
             file.write("[Twitch Streamer Status]\n")
+            for row in StreamerStatus:
+                s = "".join(map(str, row))
+                file.write(s+'\n')
+    except Exception as e:
+        print(e)
+    return
+def YouTubeStreamerList(file_path):
+    if file_path == '' or None:
+        print("Error: No streamer list selected.")
+        return
+    try:
+        textData=open(file_path,"r")
+        list1 = textData.read().split('\n')
+        for i in range(len(list1)):
+            streamer = list1[i]
+            streamerX = streamer.split(',')
+            X = CheckYouTubeStream(streamerX[0], streamerX[1])
+            if X == True:
+                StreamerStatus.append(streamerX[0] + " : ONLINE")
+            else:
+                StreamerStatus.append(streamerX[0] + " : OFFLINE")
+        with open("streamers_youtube_status.txt","a") as file:
+            file.write("[YouTube Streamer Status]\n")
             for row in StreamerStatus:
                 s = "".join(map(str, row))
                 file.write(s+'\n')
@@ -38,10 +57,35 @@ def CheckTwitchStream(streamer):
             cts = True
             return cts
         else:
-            print(streamer + " is not live streaming on Twitch.TV")
+            print(streamer + " is offline on Twitch.TV")
             cts = False
             return cts
     except Exception as e:
         print(e)
     return
-streamerList('streamers.txt', 0)
+def CheckYouTubeStream(streamer, subtype):
+    cys = None
+    try: #hqdefault_live.jpg
+        if subtype == "0": #0 = channel, 1 = vanity
+            response = urlopen("https://www.youtube.com/channel/" + streamer)
+            body = response.read()
+            decoded_body = body.decode("utf-8")
+            response.close()
+        else:
+            response = urlopen("https://www.youtube.com/c/" + streamer)
+            body = response.read()
+            decoded_body = body.decode("utf-8")
+            response.close()
+        if "hqdefault_live.jpg" in decoded_body:
+            print(streamer + " is live streaming on YouTube.com")
+            cys = True
+            return cys
+        else:
+            print(streamer + " is offline on YouTube.com")
+            cys = False
+            return cys
+    except Exception as e:
+        print(e)
+    return
+TwitchStreamerList('streamers_twitch.txt')
+YouTubeStreamerList('streamers_youtube.txt')

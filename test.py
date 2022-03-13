b4a215fc8bdb53f2c3662ec50156111ec4216f43
LeftBased <59938062+LeftBased@users.noreply.github.com>
2022-03-13 03:09:29 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,19 +1,37 @@
 from urllib.request import urlopen
-
+def streamerList(file_path, stream_platform):
+    if stream_platform == '' or None:
+        print("Error: No stream platform defined.")
+        return
+    if file_path == '' or None:
+        print("Error: No streamer list selected.")
+        return
+    try:
+        textData=open(file_path,"r")
+        list1 = textData.read().split('\n')
+        for i in range(len(list1)):
+            streamer = list1[i]
+            if stream_platform == 0:
+                CheckTwitchStream(streamer)
+    except Exception as e:
+        print(e)
+    return
 def CheckTwitchStream(streamer):
-    response = urlopen("https://m.twitch.tv/" + streamer)
-    body = response.read()
-    decoded_body = body.decode("utf-8")
-    response.close()
     cts = None
-    if "isLiveBroadcast" in decoded_body:
-        print(streamer + " is live streaming")
-        cts = True #set cts as true.
-    else:
-        print(streamer + " is not live streaming")
-        cts = False
-    return cts
-    
-CheckTwitchStream('merlinsgoat')
-CheckTwitchStream('amouranth')
-CheckTwitchStream('ninja')
+    try:
+        response = urlopen("https://twitch.tv/" + streamer)
+        body = response.read()
+        decoded_body = body.decode("utf-8")
+        response.close()
+        if "isLiveBroadcast" in decoded_body:
+            print(streamer + " is live streaming on Twitch.TV")
+            cts = True
+            return cts
+        else:
+            print(streamer + " is not live streaming on Twitch.TV")
+            cts = False
+            return cts
+    except Exception as e:
+        print(e)
+    return
+streamerList('streamers.txt', 0)

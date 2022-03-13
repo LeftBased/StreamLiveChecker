9f3095690725c63608c4661bc9e943ed5a93a553
LeftBased <59938062+LeftBased@users.noreply.github.com>
2022-03-13 04:38:16 -0700

    Update test.py

--- test.py
+++ test.py
@@ -15,37 +15,41 @@ def TwitchStreamerList(file_path):
                 StreamerStatus.append(streamer + " : ONLINE")
             else:
                 StreamerStatus.append(streamer + " : OFFLINE")
-        with open("streamers_twitch_status.txt","a") as file:
-            file.write("[Twitch Streamer Status]\n")
+        with open("streamers_twitch_status.txt","a") as file1:
+            file1.write("[Twitch Streamer Status]\n")
             for row in StreamerStatus:
                 s = "".join(map(str, row))
-                file.write(s+'\n')
+                file1.write(s+'\n')
+            file1.close()
     except Exception as e:
         print(e)
     return
+
 def YouTubeStreamerList(file_path):
     if file_path == '' or None:
         print("Error: No streamer list selected.")
         return
     try:
-        textData=open(file_path,"r")
-        list1 = textData.read().split('\n')
-        for i in range(len(list1)):
-            streamer = list1[i]
+        textData2=open(file_path,"r")
+        list2 = textData2.read().split('\n')
+        for i in range(len(list2)):
+            streamer = list2[i]
             streamerX = streamer.split(',')
             X = CheckYouTubeStream(streamerX[0], streamerX[1])
             if X == True:
                 StreamerStatus2.append(streamerX[0] + " : ONLINE")
             else:
                 StreamerStatus2.append(streamerX[0] + " : OFFLINE")
-        with open("streamers_youtube_status.txt","a") as file:
-            file.write("[YouTube Streamer Status]\n")
+        with open("streamers_youtube_status.txt","a") as file2:
+            file2.write("[YouTube Streamer Status]\n")
             for row in StreamerStatus2:
                 s = "".join(map(str, row))
-                file.write(s+'\n')
+                file2.write(s+'\n')
+            file2.close()
     except Exception as e:
         print(e)
     return
+
 def CheckTwitchStream(streamer):
     cts = None
     try:
@@ -88,5 +92,6 @@ def CheckYouTubeStream(streamer, subtype):
     except Exception as e:
         print(e)
     return
+
 TwitchStreamerList('streamers_twitch.txt')
 YouTubeStreamerList('streamers_youtube.txt')

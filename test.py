b3b96fb4c658229f6197ab0876dc0e56121afa87
LeftBased <59938062+LeftBased@users.noreply.github.com>
2022-03-13 03:28:22 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from urllib.request import urlopen
+StreamerStatus = []
 def streamerList(file_path, stream_platform):
     if stream_platform == '' or None:
         print("Error: No stream platform defined.")
@@ -12,7 +13,17 @@ def streamerList(file_path, stream_platform):
         for i in range(len(list1)):
             streamer = list1[i]
             if stream_platform == 0:
-                CheckTwitchStream(streamer)
+                X = CheckTwitchStream(streamer)
+                if X == True:
+                    StreamerStatus.append(streamer + " : ONLINE")
+                else:
+                    StreamerStatus.append(streamer + " : OFFLINE")
+        with open("Streamer_Status.txt","a") as file:
+            file.write("[Twitch Streamer Status]\n")
+            for row in StreamerStatus:
+                s = "".join(map(str, row))
+                file.write(s+'\n')
+            file.close()
     except Exception as e:
         print(e)
     return
